01723c661b3c7a4cf15224a7548d8ca826b01a1b
lvjie1 <lvjie1@xiaomi.com>
2019-08-17 20:12:54 +0800

    项目初始化

---
+++ app/src/main/java/com/cn/fweixin/FWeixinMainActivity.java
@@ -0,0 +1,13 @@
+package com.cn.fweixin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FWeixinMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fweixin_main);
+    }
+}
